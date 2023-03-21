cfd66b3aea5c93b5e6b10351a14b458b4611186f
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-03-21 23:57:07 +0100

    Remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,4 @@ const Root = () => (
   </>
 );
 
-// I've spent the last several months completely immersed in remote development, building everything from landing pages to single page applications using HTML, CSS and vanilla JavaScript. Before transitioning into software development I worked as a Computer Science Teacher for a couple of years, teaching secondary school students coding in the C language. This taught me skills like problem-solving, effective communication, multi-tasking, and more. During the last several months, I have been working on a number of projects: my portfolio, matching a page to a Figma template, landing pages, and a modular, single-page web app to manage a collection of books. Being in the top 4% of active GitHub users in my country is something that I'm really proud of.
-
 export default App;
