45bb4afffebc50237ad79e85cdd69bca3e8933eb
haftamudesta <desta451616@hotmail.com>
2023-03-21 13:32:29 +0300

    Add structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  NavLink,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Rockets from "./components/Rockets";
+import Missions from "./components/Missions";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="/Rockets" element={<Rockets />} />
+        <Route index="/" element={<Missions />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Route>
+    )
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="main__container">
+      <RouterProvider router={router} />
+    </main>
+  );
 }
 
-export default App
+const Root = () => {
+  return (
+    <>
+      <nav className="main__nav">
+        <section className="nav__link">
+          <section className="nav__link-left">
+            <img src="./src/assets/planet.png" alt="" />
+            <h1>Space Travlers' Hub</h1>
+          </section>
+          <section className="nav__link-right">
+            <NavLink className="nav__link-links rockets" to="/Rockets">
+              Rockets
+            </NavLink>
+            <NavLink className="nav__link-links missions" to="/Missions">
+              Missions
+            </NavLink>
+            <NavLink className="nav__link-links profiles" to="/Profile">
+              Profile
+            </NavLink>
+          </section>
+        </section>
+        <hr />
+      </nav>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default App;
